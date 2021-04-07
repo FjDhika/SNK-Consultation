5ebdc6d710e1adfaaf0400f51dfa2255fbc3cd79
F.A <fajarhandika12@gmail.com>
2021-04-07 22:54:16 +0700

    add searchDoctor, detailDoctor

---
+++ src/controller/doctorController.ts
@@ -0,0 +1,53 @@
+import { isEmpty } from "class-validator";
+import { Request, Response } from "express";
+import { getRepository, Like } from "typeorm";
+import { Doctor } from "../entity/doctor";
+
+export class DoctorController {
+  static searchDoctor = async (req: Request, res: Response) => {
+    const search = req.query.query || "";
+    const take = +req.query.take || 3;
+    const skip = +req.query.skip || 0;
+
+    const [doctors, total] = await getRepository(Doctor).findAndCount({
+      select: ["id", "name", "photo"],
+      where: {
+        name: Like(`%${search}%`),
+      },
+      skip: skip,
+      take: take,
+    });
+    const result = {
+      count: total,
+      status: 200,
+      data: doctors,
+    };
+    res.status(200).send(result);
+  };
+
+  static detailDoctor = async (req: Request, res: Response) => {
+    const doctorId: string = req.query.doctorId.toString();
+
+    if (isEmpty(doctorId)) {
+      res.status(401).send({
+        status: 401,
+        message: "doctorId can't be null",
+      });
+    }
+
+    const doctor = await getRepository(Doctor).find({
+      where: {
+        id: doctorId,
+      },
+    });
+
+    if (isEmpty(doctor)) {
+      res.status(401).send({
+        status: 401,
+        message: "Not Found",
+      });
+    }
+
+    res.status(200).send({ status: 200, data: doctor });
+  };
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,6 @@
 import { Router, Response, Request } from "express";
+import { DoctorController } from "./controller/doctorController";
+import { Doctor } from "./entity/doctor";
 
 const router = Router();
 
@@ -6,4 +8,7 @@ router.get("/", (req: Request, res: Response) =>
   res.status(200).send("SNK Consultation API")
 );
 
+router.get("/search", DoctorController.searchDoctor);
+
+router.get("/doctor", DoctorController.detailDoctor);
 export default router;
